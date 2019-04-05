7ca54405a313d3250c36fa9e17d85e176e184f73
bejaouij <bejaoui.je@mail.com>
2019-04-05 16:16:52 +0200

    feat($server): allow two clients in turn to send and receive messages
    Allow two clients in turn to send and recieve messages.

--- serveur.c
+++ serveur.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <unistd.h>
+
+#define MESSAGE_MAX_LENGTH 255
 
 int main() {
 	/* Initialize the server side socket */
@@ -53,6 +57,81 @@ int main() {
 
 	if(clientSocketDescriptor2 == -1) {
 		perror("Socket Accepting Error: ");
+	}
+	/********************/
+
+	/* Make the gateway between both clients to make the communication works */
+	char msg[MESSAGE_MAX_LENGTH];
+	int recvResult, sendResult;
+	int gatewayEstablished = 1;
+
+	while(gatewayEstablished == 1) {
+		recvResult = recv(clientSocketDescriptor, &msg, sizeof(char)*MESSAGE_MAX_LENGTH, 0);
+
+		if(recvResult == -1) {
+			perror("Data Reception Error: ");
+		}
+		else {
+			if(recvResult == 0) {
+				gatewayEstablished = 0;
+			}
+			else {
+				sendResult = send(clientSocketDescriptor2, &msg, sizeof(char)*((int)strlen(msg) + 1), 0);
+
+				if(sendResult == -1) {
+					perror("Data Sending Error: ");
+				}
+				else {
+					if(sendResult == 0) {
+						gatewayEstablished = 0;
+					}
+					else {
+						recvResult = recv(clientSocketDescriptor2, &msg, sizeof(char)*MESSAGE_MAX_LENGTH, 0);
+
+						if(recvResult == -1) {
+							perror("Data Reception Error: ");
+						}
+						else {
+							if(recvResult == 0) {
+								gatewayEstablished = 0;
+							}
+							else {
+								sendResult = send(clientSocketDescriptor, &msg, sizeof(char)*((int)strlen(msg) + 1), 0);
+
+								if(sendResult == -1) {
+									perror("Data Sending Error: ");
+								}
+								else {
+									if(sendResult == 0) {
+										gatewayEstablished = 0;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		if(gatewayEstablished == 0) {
+			if(close(clientSocketDescriptor) == -1) {
+				perror("Client 1 Socket Closing Error: ");
+			}
+
+			if(close(clientSocketDescriptor2) == -1) {
+				perror("Client 2 Socket Closing Error: ");
+			}
+
+			if((clientSocketDescriptor = accept(socketDescriptor, (struct sockaddr*)&clientAddress, &clientAddressLength)) == -1) {
+				perror("Client 1 Socket Accepting Error: ");
+			}
+
+			if((clientSocketDescriptor2 = accept(socketDescriptor, (struct sockaddr*)&clientAddress2, &clientAddressLength2)) == -1) {
+				perror("Client 2 Socket Accepting Error: ");
+			}
+
+			gatewayEstablished = 1;
+		}
 	}
 	/********************/
 
