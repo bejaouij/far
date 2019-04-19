bd10cef15ed8f7ad941c6b3390a7e5f27bf8e783
bejaouij <bejaoui.je@mail.com>
2019-04-19 13:29:20 +0200

    feat($server): read both clients messages simultaneously
    Initialize a thread per clients to read all messages simultaneously.
    
    Before: need to wait one client message to read other client message.
    After: recieve all clients messages simultaneously.

--- serveur.c
+++ serveur.c
@@ -24,19 +24,6 @@ typedef struct MessageTransmissionParams {
 
 /* Array(Client*) x int -> int
  * 
- * Close connection with all clients specified in parameters
- *
- * @pre: 'clientsCount' must be equal to the number of clients stored in the array.
- * @post: All client sockets must be closed.
- * @param: - clients: Array which contains all clients to disconnect.
- *         - clientsCount: Number of clients stored in 'clients' array.
- * @return: - 0 if everything is okay
- *          - -1 if something goes wrong
- */
-int closeConnection(Client* clients[], int clientsCount);
-
-/* Array(Client*) x int -> int
- * 
  * Init connection with all clients specified in parameters
  *
  * @pre: 'clientsCount' must be equal to the number of clients stored in the array.
@@ -84,19 +71,11 @@ int main() {
 	}	
 	/********************/
 
-	/* Accept two clients */
-	int gatewayEstablished = 0;
-
+	/* Initialize two clients */
 	Client* clients[2] = {
 		&((Client) { .addressLength = sizeof(struct sockaddr_in) }),
 		&((Client) { .addressLength = sizeof(struct sockaddr_in) })
 	};
-
-	while(gatewayEstablished == 0) {
-		if(initConnection(clients, 2, socketDescriptor) == 0) {
-			gatewayEstablished = 1;
-		}
-	}
 	/********************/
 
 	/* Initialize clients communication threads  */
@@ -107,79 +86,54 @@ int main() {
 		&thread1,
 		&thread2
 	};
-
-	int resThreadCreation = pthread_create(threads[0], NULL, (void*) t_messageTransmission, &((MessageTransmissionParams) {
-				.serverSocketDescriptor = socketDescriptor,
-				.senderClient = clients[0],
-				.recipientClient = clients[1]
-			}));
-
-	if(resThreadCreation != 0) {
-		perror("Thread Creation Error");
-	}
-
-	resThreadCreation = pthread_create(threads[1], NULL, (void*) t_messageTransmission, &((MessageTransmissionParams) {
-				.serverSocketDescriptor = socketDescriptor,
-				.senderClient = clients[1],
-				.recipientClient = clients[0]
-			}));
-
-	if(resThreadCreation != 0) {
-		perror("Thread Creation Error");
-	}
 	/********************/
 
 	/* Make the gateway between both clients to make the communication works */
-	char msg[MESSAGE_MAX_LENGTH];
-	int recvResult, sendResult;
-	int targetIndex = 0;
+	int resThreadCreation;
+	int gatewayEstablished;
 
 	while(1) {
-		recvResult = recv(clients[targetIndex%2]->socketDescriptor, &msg, sizeof(char)*MESSAGE_MAX_LENGTH, 0);
-
-		if(recvResult == -1) {
-			perror("Data Reception Error");
-		}
-		else {
-			if(recvResult == 0) {
-				gatewayEstablished = 0;
-			}
-			else {
-				if(strcmp(msg, "fin") == 0) {
-					gatewayEstablished = 0;
-				}
-				else {
-					sendResult = send(clients[(targetIndex + 1)%2]->socketDescriptor, &msg, sizeof(char)*((int)strlen(msg) + 1), 0);
-
-					if(sendResult == -1) {
-						perror("Data Sending Error");
-					}
-					else {
-						if(sendResult == 0) {
-							gatewayEstablished = 0;
-						}
-					}
-				}
-			}
-		}
-
-		/* Switch the target */
-		targetIndex = (targetIndex + 1)%2;
-		/********************/
+		/* Accept two clients */
+		gatewayEstablished = 0;
 
 		while(gatewayEstablished == 0) {
-			if(closeConnection(clients, 2) == 0) {
+			if(initConnection(clients, 2, socketDescriptor) == 0) {
 				gatewayEstablished = 1;
 			}
-			
-			if(gatewayEstablished == 1) {
-				if(initConnection(clients, 2, socketDescriptor) == 0) {
-					gatewayEstablished = 1;
-				}
-				
-				targetIndex = 0;
-			}
 		}
+		/********************/
+
+		/* Start communication threads routine  */
+		resThreadCreation = pthread_create(threads[0], NULL, (void*) &t_messageTransmission, &((MessageTransmissionParams) {
+					.serverSocketDescriptor = socketDescriptor,
+					.senderClient = clients[0],
+					.recipientClient = clients[1]
+				}));
+
+		if(resThreadCreation != 0) {
+			perror("Thread Creation Error");
+		}
+
+		resThreadCreation = pthread_create(threads[1], NULL, (void*) &t_messageTransmission, &((MessageTransmissionParams) {
+					.serverSocketDescriptor = socketDescriptor,
+					.senderClient = clients[1],
+					.recipientClient = clients[0]
+				}));
+
+		if(resThreadCreation != 0) {
+			perror("Thread Creation Error");
+		}
+		/********************/
+
+		/* Wait for the communication threds routine end */
+		if(pthread_join(*threads[0], 0) != 0) {
+			perror("End Thread Error");
+		}
+
+		if(pthread_join(*threads[1], 0) != 0) {
+			perror("End Thread Error");
+		}
+		/********************/
 	}
 	/********************/
 
@@ -192,39 +146,13 @@ int main() {
 	return 0;
 }
 
-int closeConnection(Client* clients[], int clientsCount) {
-	int i;
-
-	for(i = 0; i < clientsCount; i++) {
-		if(close(clients[i]->socketDescriptor) == -1) {
-			perror("Client Socket Closing Error");
-			return -1;
-		}
-	}
-
-	return 0;
-}
-
 int initConnection(Client* clients[], int clientsCount, int gatewaySocketDescriptor) {
 	int i;
-	int sendResult = 0;
 
 	for(i = 0; i < clientsCount; i++) {
 		if((clients[i]->socketDescriptor = accept(gatewaySocketDescriptor, (struct sockaddr*)&(clients[i]->address), &(clients[i]->addressLength))) == -1) {
 			perror("Client Socket Accepting Error");
 			return -1;
-		}
-	}
-
-	for(i = 0; i < clientsCount; i++) {
-		if((sendResult = send(clients[i]->socketDescriptor, &((int) {(i+1)}), 1*sizeof(int), 0)) == -1) {
-			perror("Client Index Sending Error");
-			return -1;
-		}
-		else {
-			if(sendResult == 0) {
-				return -1;
-			}
 		}
 	}
 
